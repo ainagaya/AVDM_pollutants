baa1db1256ac80f21fc87d8174219f5da66ca60f
ArmandoAC <armando.aguilar.camp@gmail.com>
2024-11-13 18:52:07 +0100

    name of columns and another try in example.py

--- example.py
+++ example.py
@@ -6,3 +6,26 @@ fetcher = DataFetcher("analisi.transparenciacatalunya.cat", "9Hbf461pXC6Lin1yqkq
 #results_filtered = fetcher.fetch_data_with_filter("municipi='Barcelona'")
 available_stations = fetcher.list_available_options_with_filter("nom_estacio", "municipi='Barcelona'")
 print(available_stations)
+
+#codi_eoi
+#nom_estacio
+#data
+#magnitud (Codi numèric que identifica el contaminant)
+#contaminant
+#unitats
+#tipus_estacio
+#area_urbana
+#codi_ine
+#municip
+#codi_comarca
+#nom comarca
+#h01
+#...
+#h24
+#altitud
+#latitud
+#longitud
+#geocoded_column
+
+contaminants = fetcher.list_available_options_with_filter("contaminant", "municipi='Barcelona'") #UNION contaminant='NO2'")
+print(contaminants)
